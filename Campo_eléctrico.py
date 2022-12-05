9fd205a0b0df29219e4e3be13a8f28348162b4ae
Manolacho73 <119118053+Manolacho73@users.noreply.github.com>
2022-12-04 21:32:26 -0300

    Figuras y tablas

--- Campo_eléctrico.py
+++ Campo_eléctrico.py
@@ -13,12 +13,14 @@ r = [100, 200, 300, 400, 100, 200, 300, 400, 100, 200, 300, 400,]
 datos_tabla = {'Carga 1': q1, 'Distancia entre cargas': r}
 df = pd.DataFrame(data=datos_tabla)
 
-df['Campo Eléctrico'] = ((df['Carga 1'] * 2 * 9 * 10 * np.exp(9))/(df['Distancia entre cargas'] * np.exp(2))) 
+df['Campo Eléctrico'] = ((df['Carga 1'] * 2 * 9 * 10 * np.exp(-9))/(df['Distancia entre cargas'] * np.exp(2))) 
 print(df)
 
 df.plot.line(x='Carga 1', y='Campo Eléctrico')
 
 plt.show()
+
+st.dataframe(df)
 
 """
 # Actividad 
@@ -30,10 +32,23 @@ plt.show()
 5. Medí la distancia entre ambas cargas con la cinta métrica.
 6. Medí la intensidad del Campo con el Voltímetro.
 7. Utilizando la Grilla Cuadriculada, medí el módulo de los vectores.
-
+8. Calculá el Campo Eléctrico usando la Ecuación a continuación.
 
 """
-st.image('./Figuras/Ejemplo.jpg')
+st.image('./Figuras/ejemplo2.png')
+
+"""
+    # Campo Eléctrico generado por 2 cargas puntuales           
+
+    $ E = \cfrac{k*q_1*q_2}{r^2} $
+
+    * $ E $ : Campo eléctrico 
+    * $ q_1 $ : Carga 1 (en $ \C (coulombios) $)
+    * $ q_2 $ : Carga 2 (en $ \C (coulombios) $)
+    * $ r $ : Distancia entre cargas eléctricas (en $ \m (metros) $)
+    * $ k $ : Constante eléctrica del medio (su valor es $ \ 9*10^{-9} N*m^2*C^{-2} $)
+    
+"""
 
 url = 'https://phet.colorado.edu/sims/html/charges-and-fields/latest/charges-and-fields_es.html'
 
